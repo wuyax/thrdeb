eaf7a2ae6ef0c471977d2023fb95dd572633d5de
wuyax <pengqingkai@gmail.com>
2020-06-10 18:51:25 +0800

    修改

--- src/views/pfc.vue
+++ src/views/pfc.vue
@@ -4,17 +4,21 @@
       @click="clickMe">click me native</button>
     <button class="bg-blue-500 px-4 py-1 rounded-sm active:bg-blue-600 ml-10"
       v-pfc='clickMe'>click me debounced</button>
+    <div class="py-4 text-6xl text-gray-200"
+      @click="reset">{{times}}</div>
   </div>
 </template>
 <script>
 // https://cn.vuejs.org/v2/style-guide/#组件-实例的选项的顺序-推荐
 export default {
-  name: '',
+  name: 'Pfc',
   components: {},
   model: {},
   props: {},
   data() {
-    return {}
+    return {
+      times: 0,
+    }
   },
   computed: {},
   watch: {},
@@ -30,6 +34,9 @@ export default {
         .catch(() => {
           console.log('error')
         })
+        .finally(() => {
+          this.times++
+        })
     },
     mockData() {
       return new Promise((resolve, reject) => {
@@ -42,6 +49,9 @@ export default {
           }
         }, 300)
       })
+    },
+    reset() {
+      this.times = 0
     }
   }
 }
